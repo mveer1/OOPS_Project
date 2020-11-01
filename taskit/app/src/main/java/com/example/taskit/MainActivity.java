2bb8f26a76366c6a03182c3594abfba3fef3c865
GarvitK01 <garvit.kashyap01@gmail.com>
2020-11-01 09:23:36 +0530

    Facebook/Google Sign-In with in new UI Added, New Branch

--- taskit/app/src/main/java/com/example/taskit/MainActivity.java
+++ taskit/app/src/main/java/com/example/taskit/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.taskit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void to_login(View view) {
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
     }
 }
