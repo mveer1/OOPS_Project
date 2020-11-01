f45ad3e2efee763202ff451e69dd9bba689d68e1
Kshitij Upadhyay <kshitijupadhyay15@yahoo.com>
2020-11-01 13:02:42 +0530

    UI login final

---
+++ OOPS_Project/app/src/main/java/com/example/oops_project/verificationpage.java
@@ -0,0 +1,14 @@
+package com.example.oops_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class verificationpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verificationpage);
+    }
+}
