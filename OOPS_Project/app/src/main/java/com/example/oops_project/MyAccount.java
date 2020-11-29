480f33a0cc3c699f09ac72dba15d131cd9e9d781
Mahavir Chaudhari <mveer.rc@gmail.com>
2020-11-29 10:55:53 +0530

    My account page done, except for getString non-static error

--- OOPS_Project/app/src/main/java/com/example/oops_project/MyAccount.java
+++ OOPS_Project/app/src/main/java/com/example/oops_project/MyAccount.java
@@ -3,22 +3,46 @@ package com.example.oops_project;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MyAccount extends AppCompatActivity {
-    FirebaseFirestore fStore = FirebaseFirestore.getInstance();
-    private TextView mTextView;
+    FirebaseFirestore fStore;
+    TextView un, ph, em, pr;
+    FirebaseAuth fAuth;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
-        mTextView = (TextView) findViewById(R.id.text);
+        un = (TextView) findViewById(R.id.username);
+        ph = (TextView) findViewById(R.id.phone);
+        em = (TextView) findViewById(R.id.email);
+        pr = (TextView) findViewById(R.id.profession);
 
+        userId = fAuth.getCurrentUser().getUid();
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                un.setText(DocumentSnapshot.getString("username"));
+                ph.setText(DocumentSnapshot.getString("phone"));
+                em.setText(DocumentSnapshot.getString("email"));
+                pr.setText(DocumentSnapshot.getString("profession"));
+
+            }
+        });
     }
 }
